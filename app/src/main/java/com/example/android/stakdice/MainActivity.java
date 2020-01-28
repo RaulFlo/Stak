0931ac13b7ca12ca767c2565164574c20fc31764
Raul Flores <raulflo2012@gmail.com>
2020-01-28 13:00:05 -0600

    Fixed Constraints and no orientation change

--- app/src/main/java/com/example/android/stakdice/MainActivity.java
+++ app/src/main/java/com/example/android/stakdice/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.android.stakdice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnEasy,btnMedium,btnHard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bind views
+        btnEasy = findViewById(R.id.btnEasy);
+        btnMedium = findViewById(R.id.btnMedium);
+        btnHard = findViewById(R.id.btnHard);
+
+
+        //passing intents
+        btnEasy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameMatt.class);
+                startActivity(intent);
+            }
+        });
+        btnMedium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameMatt.class);
+                startActivity(intent);
+            }
+        });
+        btnHard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameMatt.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
