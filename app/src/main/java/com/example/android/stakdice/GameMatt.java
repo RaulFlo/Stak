eb9b14f32b90e559b319f4cc9875d296d9a0e9dd
Raul Flores <raulflo2012@gmail.com>
2020-01-29 14:51:07 -0600

    Add Dice button roll

--- app/src/main/java/com/example/android/stakdice/GameMatt.java
+++ app/src/main/java/com/example/android/stakdice/GameMatt.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class GameMatt extends AppCompatActivity {
+
+    private ImageView imageViewDice;
+    private Random rng = new Random();
+    private Button rollButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +23,7 @@ public class GameMatt extends AppCompatActivity {
         setContentView(R.layout.activity_game_matt);
 
         Intent intent = getIntent();
-      StakCard stakCard= (StakCard) intent.getSerializableExtra("StakCard");
+        StakCard stakCard = (StakCard) intent.getSerializableExtra("StakCard");
 
         int imageExtra = stakCard.getImageResource();
         String nameExtra = stakCard.getCardName();
@@ -32,8 +40,10 @@ public class GameMatt extends AppCompatActivity {
         TextView textViewName = findViewById(R.id.stak_view_card_name);
         TextView textViewStrength = findViewById(R.id.stak_view_strength);
         TextView textViewToughness = findViewById(R.id.stak_view_toughness);
-        TextView textViewAgility= findViewById(R.id.stak_view_agility);
+        TextView textViewAgility = findViewById(R.id.stak_view_agility);
         TextView textViewKnowledge = findViewById(R.id.stak_view_knowledge);
+        imageViewDice = findViewById(R.id.image_view_dice);
+        rollButton = findViewById(R.id.button_roll);
 
         //link extras to views
         imageView.setImageResource(imageExtra);
@@ -43,6 +53,38 @@ public class GameMatt extends AppCompatActivity {
         textViewAgility.setText(String.valueOf(agilityExtra));
         textViewKnowledge.setText(String.valueOf(knowledgeExtra));
 
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rollDice();
+            }
+        });
 
+
+    }
+
+    private void rollDice() {
+        int randomNumber = rng.nextInt(6) + 1;
+
+        switch (randomNumber) {
+            case 1:
+                imageViewDice.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                imageViewDice.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                imageViewDice.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                imageViewDice.setImageResource(R.drawable.dice4);
+                break;
+            case 5:
+                imageViewDice.setImageResource(R.drawable.dice5);
+                break;
+            case 6:
+                imageViewDice.setImageResource(R.drawable.dice6);
+                break;
+        }
     }
 }
