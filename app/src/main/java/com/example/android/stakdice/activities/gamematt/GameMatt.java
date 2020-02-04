10c6372ff5c241fb0ac0496f333e60c3413934c0
Raul Flores <raulflo2012@gmail.com>
2020-02-03 18:40:14 -0600

    Dialog xml

--- app/src/main/java/com/example/android/stakdice/activities/gamematt/GameMatt.java
+++ app/src/main/java/com/example/android/stakdice/activities/gamematt/GameMatt.java
@@ -123,12 +123,19 @@ public class GameMatt extends AppCompatActivity {
 
             Toast.makeText(this, "Passed", Toast.LENGTH_SHORT).show();
 
+            openPassDialog();
+
             //Change the isBeaten on the card to true, commented out for now
             stakCard.setBeaten(true);
             gameMattViewModel.update(stakCard);
         } else {
             Toast.makeText(this, "failed", Toast.LENGTH_SHORT).show();
         }
+
+    }
+
+    private void openPassDialog() {
+
 
     }
 
